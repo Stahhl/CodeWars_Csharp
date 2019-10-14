0819141b31ee26f4134da9d4158dc4db60ea272a
Arvostahl <arvostahl@gmail.com>
2019-10-14 21:45:30 +0200

    where_my_anagrams_at, validbraces

---
+++ CodeWars1/ValidBraces.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using NUnit.Framework;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace CodeWars1
+{
+    //https://www.codewars.com/kata/valid-braces/train/csharp
+    class ValidBraces
+    {
+        public static bool validBraces(String braces)
+        {
+            var list = braces.ToList();
+            var startBrace = new char[] { '(', '{', '[' };
+            var closeBrace = new char[] { ')', '}', ']' };
+            var starting = list.Where(x => startBrace.Contains(x));
+            var closing = list.Where(x => closeBrace.Contains(x));
+
+            if (braces.Length % 2 != 0 || (starting.Count() != closing.Count()))
+                return false;
+
+            for (int a = 0; a < list.Count() - 1; a++)
+            {
+                var current = list[a];
+                var next = list[a + 1];
+
+                if(startBrace.Contains(current) || closeBrace.Contains(next))
+                {
+                    var currentIndex = Array.IndexOf(startBrace, current);
+                    var nextIndex = Array.IndexOf(closeBrace, next);
+
+                    if(currentIndex == nextIndex)
+                    {
+                        list.Remove(current);
+                        list.Remove(next);
+
+                        a = -1;
+                    }
+                }
+            }
+
+            return list.Count == 0 ? true : false;
+        }
+
+        [TestFixture]
+        public class BraceTests
+        {
+
+            [Test]
+            public void Test1()
+            {
+                Assert.AreEqual(true, validBraces("()"));
+            }
+            [Test]
+            public void Test2()
+            {
+
+                Assert.AreEqual(false, validBraces("[(])"));
+            }
+            [Test]
+            public void Test3()
+            {
+
+                Assert.AreEqual(true, validBraces("(){}[]"));
+            }
+            [Test]
+            public void Test4()
+            {
+
+                Assert.AreEqual(true, validBraces("([{}])"));
+            }
+            [Test]
+            public void Test5()
+            {
+
+                Assert.AreEqual(false, validBraces("(}"));
+            }
+            [Test]
+            public void Test6()
+            {
+
+                Assert.AreEqual(false, validBraces("[(])"));
+            }
+            [Test]
+            public void Test7()
+            {
+
+                Assert.AreEqual(false, validBraces("[({})](]"));
+            }
+            [Test]
+            public void Test8()
+            {
+                Assert.AreEqual(true, validBraces("((((((((((((((((((((()))))))))))))))))))))"));
+            }
+            [Test]
+            public void Test9()
+            {
+                Assert.AreEqual(true, validBraces("(())[]"));
+            }
+        }
+    }
+}
---
+++ CodeWars1/Where_my_anagrams_at.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars1
+{
+    class Where_my_anagrams_at
+    {
+        public static List<string> Anagrams(string word, List<string> words)
+        {
+            List<string> result = new List<string>();
+
+            var a = new String(word.OrderBy(x => x).ToArray());
+
+            foreach (var item in words)
+            {
+                var b = new String(item.OrderBy(y => y).ToArray());
+
+                if (a == b)
+                    result.Add(item);
+            }
+
+            return result;
+        }
+        [TestFixture]
+        public class SolutionTest
+        {
+            [Test]
+            public void SampleTest()
+            {
+                Assert.AreEqual(new List<string> { "a" }, Anagrams("a", new List<string> { "a", "b", "c", "d" }));
+                Assert.AreEqual(new List<string> { "carer", "arcre", "carre" }, Anagrams("racer", new List<string> { "carer", "arcre", "carre", "racrs", "racers", "arceer", "raccer", "carrer", "cerarr" }));
+            }
+        }
+    }
+}
