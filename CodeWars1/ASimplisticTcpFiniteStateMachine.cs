79e7b0dec9a53b75ea38c769c58a6394cc511f40
Arvid Ståhl Strömberg <arvostahl@gmail.com>
2020-01-14 16:07:44 +0100

    tcp

---
+++ CodeWars1/ASimplisticTcpFiniteStateMachine.cs
@@ -0,0 +1,136 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CodeWars1
+{
+    //CLOSED, LISTEN, SYN_SENT, SYN_RCVD, ESTABLISHED, CLOSE_WAIT, LAST_ACK, FIN_WAIT_1, FIN_WAIT_2, CLOSING, TIME_WAIT
+    enum State
+    {
+        ERROR,
+        CLOSED,
+        LISTEN,
+        SYN_SENT,
+        SYN_RCVD,
+        ESTABLISHED,
+        CLOSE_WAIT,
+        LAST_ACK,
+        FIN_WAIT_1,
+        FIN_WAIT_2,
+        CLOSING,
+        TIME_WAIT,
+    }
+    //APP_PASSIVE_OPEN, APP_ACTIVE_OPEN, APP_SEND, APP_CLOSE, APP_TIMEOUT, RCV_SYN, RCV_ACK, RCV_SYN_ACK, RCV_FIN, RCV_FIN_ACK
+    enum Event
+    {
+        APP_PASSIVE_OPEN,
+        APP_ACTIVE_OPEN,
+        APP_SEND,
+        APP_CLOSE,
+        APP_TIMEOUT,
+        RCV_SYN,
+        RCV_ACK,
+        RCV_SYN_ACK,
+        RCV_FIN,
+        RCV_FIN_ACK,
+    }
+    public class TCP
+    {
+        //https://www.codewars.com/kata/54acc128329e634e9a000362/train/csharp
+        public static string TraverseStates(string[] events)
+        {
+            try
+            {
+                var tuples = SetupRelationShips();
+                var eList = new List<Event>();
+                var state = State.CLOSED;
+
+                foreach (var s in events)
+                {
+                    eList.Add((Event)Enum.Parse(typeof(Event), s));
+                }
+                foreach (var e in eList)
+                {
+                    state = tuples.Where(x => x.Item1 == state && x.Item2 == e).Select(y => y.Item3).FirstOrDefault();
+
+                    if (state == State.ERROR)
+                        throw new Exception("ERROR");
+                }
+
+                return state.ToString();
+            }
+            catch(Exception e)
+            {
+                return State.ERROR.ToString();
+            }
+        }
+        private static List<Tuple<State, Event, State>> SetupRelationShips()
+        {
+            var tuples = new List<Tuple<State, Event, State>>();
+
+            //CLOSED
+            tuples.Add(new Tuple<State, Event, State>(State.CLOSED, Event.APP_PASSIVE_OPEN, State.LISTEN));
+            tuples.Add(new Tuple<State, Event, State>(State.CLOSED, Event.APP_ACTIVE_OPEN, State.SYN_SENT));
+            //LISTEN
+            tuples.Add(new Tuple<State, Event, State>(State.LISTEN, Event.RCV_SYN, State.SYN_RCVD));
+            tuples.Add(new Tuple<State, Event, State>(State.LISTEN, Event.APP_SEND, State.SYN_SENT));
+            tuples.Add(new Tuple<State, Event, State>(State.LISTEN, Event.APP_CLOSE, State.CLOSED));
+            //SYN_RCVD
+            tuples.Add(new Tuple<State, Event, State>(State.SYN_RCVD, Event.APP_CLOSE, State.FIN_WAIT_1));
+            tuples.Add(new Tuple<State, Event, State>(State.SYN_RCVD, Event.RCV_ACK, State.ESTABLISHED));
+            //SYN_SENT
+            tuples.Add(new Tuple<State, Event, State>(State.SYN_SENT, Event.RCV_SYN, State.SYN_RCVD));
+            tuples.Add(new Tuple<State, Event, State>(State.SYN_SENT, Event.RCV_SYN_ACK, State.ESTABLISHED));
+            tuples.Add(new Tuple<State, Event, State>(State.SYN_SENT, Event.APP_CLOSE, State.CLOSED));
+            //ESTABLISHED
+            tuples.Add(new Tuple<State, Event, State>(State.ESTABLISHED, Event.APP_CLOSE, State.FIN_WAIT_1));
+            tuples.Add(new Tuple<State, Event, State>(State.ESTABLISHED, Event.RCV_FIN, State.CLOSE_WAIT));
+            //FIN_WAIT
+            tuples.Add(new Tuple<State, Event, State>(State.FIN_WAIT_1, Event.RCV_FIN, State.CLOSING));
+            tuples.Add(new Tuple<State, Event, State>(State.FIN_WAIT_1, Event.RCV_FIN_ACK, State.TIME_WAIT)); //
+            tuples.Add(new Tuple<State, Event, State>(State.FIN_WAIT_1, Event.RCV_ACK, State.FIN_WAIT_2));
+            //CLOSING
+            tuples.Add(new Tuple<State, Event, State>(State.CLOSING, Event.RCV_ACK, State.TIME_WAIT)); //
+            //FIN_WAIT 2
+            tuples.Add(new Tuple<State, Event, State>(State.FIN_WAIT_2, Event.RCV_FIN, State.TIME_WAIT)); //
+            //TIME_WAIT
+            tuples.Add(new Tuple<State, Event, State>(State.TIME_WAIT, Event.APP_TIMEOUT, State.CLOSED));
+            //CLOSE_WAIT
+            tuples.Add(new Tuple<State, Event, State>(State.CLOSE_WAIT, Event.APP_CLOSE, State.LAST_ACK));
+            //LAST_ACK
+            tuples.Add(new Tuple<State, Event, State>(State.LAST_ACK, Event.RCV_ACK, State.CLOSED));
+
+            return tuples;
+        }
+    }
+
+    public class TcpTest
+    {
+        [Test]
+        public void Test01()
+        {
+            Assert.AreEqual("CLOSE_WAIT", TCP.TraverseStates(new[] { "APP_ACTIVE_OPEN", "RCV_SYN_ACK", "RCV_FIN" }));
+        }
+        [Test]
+        public void Test02()
+        {
+            Assert.AreEqual("ESTABLISHED", TCP.TraverseStates(new[] { "APP_PASSIVE_OPEN", "RCV_SYN", "RCV_ACK" }));
+        }
+        [Test]
+        public void Test03()
+        {
+            Assert.AreEqual("LAST_ACK", TCP.TraverseStates(new[] { "APP_ACTIVE_OPEN", "RCV_SYN_ACK", "RCV_FIN", "APP_CLOSE" }));
+        }
+        [Test]
+        public void Test04()
+        {
+            Assert.AreEqual("SYN_SENT", TCP.TraverseStates(new[] { "APP_ACTIVE_OPEN" }));
+        }
+        [Test]
+        public void Test05()
+        {
+            Assert.AreEqual("ERROR", TCP.TraverseStates(new[] { "APP_PASSIVE_OPEN", "RCV_SYN", "RCV_ACK", "APP_CLOSE", "APP_SEND" }));
+        }
+    }
+}
--- CodeWars1/TenPointBowling.cs
+++ CodeWars1/TenPointBowling.cs
@@ -12,10 +12,11 @@ namespace CodeWars1
     {
         public static int BowlingScore(string frames)
         {
-            var frameList = frames.Split(" ").Select(x => x.Trim()).ToList();
+            var frameList = frames.Split(" ").Select(x => x.Trim().ToUpper()).ToList();
 
             var frameCells = new List<int[,,]>();
             var frameScores = new List<int>();
+            var stepScores = new List<int>();
 
             for (int i = 0; i < frameList.Count; i++)
             {
@@ -28,6 +29,7 @@ namespace CodeWars1
                 int bonus = i < 9 ? getFrameBonus(frameList, frameCells, i) : 0;
 
                 frameScores.Add(getCellValue(frameCells[i]) + bonus);
+                stepScores.Add(frameScores.Sum());
             }
 
             int result = frameScores.Sum();
@@ -37,17 +39,34 @@ namespace CodeWars1
         private static int getFrameBonus(List<string> sList, List<int[,,]> cList, int i)
         {
             var sValue = sList[i];
-
-            var max = 9;
-
-            var s1 =  i + 1 >= max ? sList[9][0].ToString() : sList[i + 1];
-            var s2 = i + 2 >= max ? sList[9][1].ToString() : sList[i + 2];
-
-            var c1 =  i + 1 >= max ? new int[cList[9].GetLength(0), 0, 0] :  cList[i + 1];
-            var c2 = i + 2 >= max ? new int[cList[9].GetLength(1), 0, 0] : cList[i + 2];
-
-
             int result = 0;
+            int max = 9;
+
+            var s1 =  i + 1 >= max ? null : sList[i + 1];
+            var s2 = i + 2 >= max ? null : sList[i + 2];
+
+            var c1 =  i + 1 >= max ? null :  cList[i + 1];
+            var c2 = i + 2 >= max ? null : cList[i + 2];
+
+            if(i == 7)
+            {
+                s2 = sList[9][0].ToString();
+                c2 = new int[cList[9].GetLength(0), 0, 0];
+            }
+            if(i == 8)
+            {
+                s1 = sList[9][0].ToString();
+                c1 = new int[cList[9].GetLength(0), 0, 0];
+
+                s2 = sList[9][1].ToString();
+                c2 = new int[cList[9].GetLength(1), 0, 0];
+
+                if (sValue.Contains("X") && s2.Contains("/"))
+                    result += c2.GetLength(0);
+                if (sValue.Contains("X") && int.TryParse(s1 + s2, out int x))
+                    c1 = new int[c1.GetLength(0), c2.GetLength(0),0];
+            }
+
 
             if(sValue.Contains("X"))
             {
@@ -59,11 +78,6 @@ namespace CodeWars1
             if(sValue.Contains("/"))
             {
                 result += c1.GetLength(0);
-            }
-
-            if(i + 2 >= max)
-            {
-
             }
 
             return result;
@@ -95,161 +109,145 @@ namespace CodeWars1
     public class TenPointBowlingTest
     {
         [Test]
-        public void Test1()
+        public void Test01()
         {
-            Console.WriteLine("Maybe this bowler should put bumpers on...\n ");
-            Assert.AreEqual(20, TenPointBowling.BowlingScore("11 11 11 11 11 11 11 11 11 11"));
+            Assert.AreEqual(20, TenPointBowlingBestPractice.BowlingScore("11 11 11 11 11 11 11 11 11 11"));
         }
         [Test]
-        public void Test2()
+        public void Test02()
         {
-            Console.WriteLine("Woah! Perfect game!");
-            Assert.AreEqual(300, TenPointBowling.BowlingScore("X X X X X X X X X XXX"));
+            Assert.AreEqual(300, TenPointBowlingBestPractice.BowlingScore("X X X X X X X X X XXX"));
         }
         [Test]
-        public void Test3()
+        public void Test03()
         {
-            Assert.AreEqual(145, TenPointBowling.BowlingScore("9/ 44 X 32 7/ 5/ 71 12 X XXX"));
+            Assert.AreEqual(145, TenPointBowlingBestPractice.BowlingScore("9/ 44 X 32 7/ 5/ 71 12 X XXX"));
         }
         [Test]
-        public void Test4()
+        public void Test04()
         {
-            Assert.AreEqual(30, TenPointBowling.BowlingScore("X 11 11 11 11 11 11 11 11 11"));
+            Assert.AreEqual(30, TenPointBowlingBestPractice.BowlingScore("X 11 11 11 11 11 11 11 11 11"));
         }
         [Test]
-        public void Test5()
+        public void Test05()
         {
-            Console.WriteLine("Woah! Perfect game!");
-            Assert.AreEqual(281, TenPointBowling.BowlingScore("X X X X X X X X X X51"));
+            Assert.AreEqual(281, TenPointBowlingBestPractice.BowlingScore("X X X X X X X X X X51"));
+        }
+        [Test]
+        public void Test06()
+        {
+            Assert.AreEqual(150, TenPointBowlingBestPractice.BowlingScore("5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/ 5/5"));
+        }
+        [Test]
+        public void Test07()
+        {
+            Assert.AreEqual(133, TenPointBowlingBestPractice.BowlingScore("X 5/ X 72 90 X 3/ 54 35 22"));
+        }
+        [Test]
+        public void Test08()
+        {
+            Assert.AreEqual(88, TenPointBowlingBestPractice.BowlingScore("X 00 X 5/ 00 42 6/ X 10 X00"));
+        }
+        [Test]
+        public void Test09()
+        {
+            Assert.AreEqual(90, TenPointBowlingBestPractice.BowlingScore("X X X X 00 00 00 00 00 00"));
+        }
+        [Test]
+        public void Test10()
+        {
+            Assert.AreEqual(40, TenPointBowlingBestPractice.BowlingScore("5/ X 00 X 00 00 00 00 00 00"));
+        }
+        [Test]
+        public void Test11()
+        {
+            Assert.AreEqual(30, TenPointBowlingBestPractice.BowlingScore("00 00 00 00 00 00 00 00 00 XXX"));
+        }
+        [Test]
+        public void Test12()
+        {
+            Assert.AreEqual(85, TenPointBowlingBestPractice.BowlingScore("0/ 10 21 32 43 54 60 71 9/ X32"));
+        }
+        [Test]
+        public void Test13()
+        {
+            Assert.AreEqual(40, TenPointBowlingBestPractice.BowlingScore("x 00 00 00 00 00 00 00 x 0/0"));
+        }
+        [Test]
+        public void Test14()
+        {
+            Assert.AreEqual(95, TenPointBowlingBestPractice.BowlingScore("70 07 X X 01 10 X X 04 2/0"));
+        }
+        [Test]
+        public void Test15()
+        {
+            Assert.AreEqual(265, TenPointBowlingBestPractice.BowlingScore("X X X X X X X X 5/ X5/"));
+        }
+        [Test]
+        public void Test16()
+        {
+            Assert.AreEqual(70, TenPointBowlingBestPractice.BowlingScore("00 00 00 00 00 00 00 5/ X XX0"));
+        }
+        [Test]
+        public void Test17()
+        {
+            Assert.AreEqual(150, TenPointBowlingBestPractice.BowlingScore("x x x x x x 00 00 00 00"));
+        }
+        [Test]
+        public void Test18()
+        {
+            Assert.AreEqual(170, TenPointBowlingBestPractice.BowlingScore("x x x x x x 00 00 00 5/X"));
+        }
+        [Test]
+        public void Test19()
+        {
+            Assert.AreEqual(50, TenPointBowlingBestPractice.BowlingScore("00 00 00 00 00 00 00 X X 44"));
         }
     }
 }
 
-
-//          for (int i = 0; i<fArray.Length; i++)
-//            {
-//                string f = fArray[i].ToUpper().Trim();
-//int strikeMulti = 1;
-
-//fScore = 0;
-
-//                if(strikes > 0)
-//                {
-//                    strikeMulti = 3;
-
-//                    strikes--;
-//                }
-//                if (f == "X")
-//                {
-//                    fScore += 10;
-
-//                    strikes += 2;
-//                }
-//                if (f.Contains('/') == true)
-//                {
-//                    f = "/";
-//                    fScore += 10;
-//                    spares++;
-//                }
-//                if(f == "XXX")
-//                {
-//                    fScore += 30;
-//                    strikeMulti = 1;
-//                    strikes = 0;
-//                }
-//                if (int.TryParse(f, out int digit) == true)
-//                {
-//                    foreach (var c in f)
-//                    {
-//                        int spareMulti = 1;
-
-//                        if (spares > 0)
-//                        {
-//                            spareMulti = 2;
-//                            spares--;
-//                        }
-
-//                        fScore += (int) Char.GetNumericValue(c) * spareMulti;
-//                    }
-//                }
-
-//                tScore += fScore* strikeMulti;
-//            }
+public static class TenPointBowlingBestPractice
+{
+    public static int AToI(char c)
+    {
+        return c == 'X' ? 10 : int.Parse(c.ToString());
+    }
+    public static int BowlingScore(string frames)
+    {
+        int frameIdx = 0;
+        int score = 0;
+        string rolls = frames.ToUpper().Replace(" ", "");
+        int totalRolls = rolls.Length;
+        for (int t = 0; t < totalRolls && frameIdx < 20; ++t)
+        {
+            if (rolls[t] == 'X')
+            {
+                score += 10;
+                score += AToI(rolls[t + 1]);
+                if (rolls[t + 2] == '/')
+                {
+                    score += 10 - AToI(rolls[t + 1]);
+                }
+                else
+                {
+                    score += AToI(rolls[t + 2]);
+                }
+                frameIdx += 2;
+            }
+            else if (rolls[t] == '/')
+            {
+                score += 10 - AToI(rolls[t - 1]);
+                score += AToI(rolls[t + 1]);
+                frameIdx += 1;
+            }
+            else
+            {
+                score += AToI(rolls[t]);
+                frameIdx += 1;
+            }
+        }
 
 
-
-//int tScore = 0;
-
-//string[] fArray = frames.Split(" ");
-
-//var frameScore = new List<int[,]>();
-
-//var totalScore = new List<int>();
-//var frameMulti = new List<string>();
-
-//            foreach (var f in fArray)
-//            {
-//                int[,] s = new int[0, 0];
-//string m = "";
-
-//                if (f.Contains("X"))
-//                {
-//                    s = new int[10, 0];
-//                    m = "X";
-
-//                    if (f.Length > 1)
-//                    {
-//                        tScore += 30;
-//                        //foreach (var x in f)
-//                        //{
-//                        //    //m = "";
-
-//                        //    //frameScore.Add(s);
-//                        //    //frameMulti.Add(m);
-//                        //    tScore += 10;
-//                        //}
-//                        //continue;
-//                    }
-//                }
-//                if (f.Contains("/"))
-//                {
-//                    m = "/";
-
-//                    s = new int[10, 0];
-//                }
-//                else if (int.TryParse(f, out int digit))
-//                {
-//                    var d1 = (int)Char.GetNumericValue(f[0]);
-//var d2 = (int)Char.GetNumericValue(f[1]);
-
-//s = new int[d1, d2];
-//                }
-
-//                frameScore.Add(s);
-//                frameMulti.Add(m);
-//            }
-
-//            int max = frameScore.Count;
-
-//            for (int i = 0; i<max; i++)
-//            {
-//                int fScore = cellValue(frameScore[i]);
-
-//                if (i + 1 < max && frameMulti[i] == "X")
-//                {
-//                    fScore += cellValue(frameScore[i + 1]);
-//                }
-//                if (i + 2 < max && frameMulti[i + 1] == "X")
-//                {
-//                    fScore += cellValue(frameScore[i + 2]);
-//                }
-//                if (i + 1 < max && frameMulti[i] == "/")
-//                {
-
-//                }
-
-//                tScore += fScore;
-//                totalScore.Add(fScore);
-//            }
-
-//            return tScore;
+        return score;
+    }
+}
